8901863a6d38b2f72ab193c99812a7918a356ab5
Anirudh <aayanamanirudh@gmail.com>
2024-06-30 02:04:22 +0530

    test3

--- autopush.sh
+++ autopush.sh
@@ -8,15 +8,20 @@ set -e
 set -o pipefail
 
 confirmation="Y"
+
+read -p "Enter the branch you want to push to: " branch
 echo "About to add files, commit, and push to GitHub repo"
-read -p "Do you confirm? (Y/n)" confirmation
+read -p "Do you confirm? (Y/n) " confirmation
+
+git checkout "$branch"
+
 
 if [[ "$confirmation" = "Y" || "$confirmation" = "y" || -z "$confirmation" ]]; then
     read -p "Enter the message for commit: " message
     git add .
     git commit -m "$message"
     git pull origin main --rebase
-    git push
+    git push -u origin "$branch"
 
     echo "Pushed successfully"
 else
