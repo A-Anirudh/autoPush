6e9e333a92cb47f1f8364a519ec33c3bca8de7fa
Anirudh <aayanamanirudh@gmail.com>
2024-06-30 01:52:00 +0530

    test

--- autopush.sh
+++ autopush.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
-
 #################
-# Automatically adds all the files, commits them and pushes them to github repo. YOu need to initially configure it once
+# Automatically adds all the files, commits them and pushes them to github repo. You need to initially configure it once
 # Author: Anirudh
 # Date: Jun 30 2024
 ################
 
 set -e
 set -o pipefail
+
 confirmation="Y"
-echo "About to add files, commit and push to github repo"
+echo "About to add files, commit, and push to GitHub repo"
 read -p "Do you confirm? (Y/n)" confirmation
 
-if [ "$confirmation"="Y" ] || [ "$confirmation"="y" ] || [ -z "$confirmation" ]; then
-    read -p "Enter the message for commit" message
+if [[ "$confirmation" = "Y" || "$confirmation" = "y" || -z "$confirmation" ]]; then
+    read -p "Enter the message for commit: " message
     git add .
     git commit -m "$message"
     git push
 
     echo "Pushed successfully"
 else
-    echo "Not pushing or commiting anything. Exit"
+    echo "Not pushing or committing anything. Exit"
 fi
 
-
-echo "completed program"
+echo "Completed program"
